5d82c9d582417b48b33d1445758bbb61784232b2
487346 <fizzarehan@hotmail.com>
2025-05-10 04:34:50 +0500

    Youtube Comment Analysis.py

--- Youtube Comment Analysis.py
+++ Youtube Comment Analysis.py
@@ -24,7 +24,7 @@ st.title('Vibes Pie - YouTube Sentiment Analysis Dashboard')
 st.write('Unmasking the true sentiments through comments!')
 
 # YouTube API Key and Configurations
-API_KEY = 'YOUR_API_KEY'  # Replace with your own YouTube API Key
+API_KEY = 'AIzaSyD5-RtE9nM-wgOXCSnQsmz6CuN4dnDJ7bE'  # Replace with your own YouTube API Key
 youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 # User Input for YouTube Video URL
