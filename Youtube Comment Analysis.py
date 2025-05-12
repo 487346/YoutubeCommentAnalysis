40d6ec3f1cd65c3bdb1d32829bfc208a0f12fa25
487346 <fizzarehan@hotmail.com>
2025-05-12 13:42:28 +0500

    Update Youtube Comment Analysis.py

--- Youtube Comment Analysis.py
+++ Youtube Comment Analysis.py
@@ -251,10 +251,13 @@ if video_url:
         
         # Plot the metrics with percentage display
         with col2:
-            st.write("Metrics DataFrame Preview:")
-            st.write(metrics_df)
+            st.subheader('Confusion Matrix Breakdown')
+            plt.figure(figsize=(7, 5))
+            
             # Plotting each bar separately for better visibility
             bar_width = 0.2
+            st.write("Metrics DataFrame Preview:")
+            st.write(metrics_df)
             st.write("Length of Classes:", len(metrics_df['Class']))
             positions = np.arange(len(metrics_df['Class'])) if len(metrics_df) > 0 else np.array([0])
             
